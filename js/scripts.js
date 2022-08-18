f1ff693b7164cd304ac8a802a1208962e6740464
Emil Anton <emilbstn@gmail.com>
2022-08-17 18:58:13 -0700

    add a multiplication function

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,14 @@ function subtract(number1, number2) {
   return number1 - number2;
 }
 
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The sum is "+ add(number1, number2) + ".");
-window.alert("The result is " + subtract(number1, number2) + ".")
+window.alert("The addition of your numbers equals "+ add(number1, number2) + ".");
+window.alert("The subtraction of your numbers equals " + subtract(number1, number2) + ".")
+window.alert("The multiplication of your numbers equals " + multiply(number1, number2) + ".")
