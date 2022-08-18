db0da338c9c853c44b6cccd9a0442a5d5e55915f
Emil Anton <emilbstn@gmail.com>
2022-08-17 18:55:18 -0700

    add a subtraction function

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,13 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert("The sum is "+ add(number1, number2) + ".");
+window.alert("The result is " + subtract(number1, number2) + ".")
