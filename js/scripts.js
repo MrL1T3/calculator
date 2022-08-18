1c5585e1a8722b36a1b1f6984108a537d551aac0
Emil Anton <emilbstn@gmail.com>
2022-08-17 19:01:17 -0700

    add a division function

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,10 @@ function multiply(number1, number2) {
   return number1 * number2;
 }
 
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
@@ -18,3 +22,4 @@ const number2 = parseInt(prompt("Enter another number:"));
 window.alert("The addition of your numbers equals "+ add(number1, number2) + ".");
 window.alert("The subtraction of your numbers equals " + subtract(number1, number2) + ".")
 window.alert("The multiplication of your numbers equals " + multiply(number1, number2) + ".")
+window.alert("The division of your numbers equals " + divide(number1, number2) + ".")
